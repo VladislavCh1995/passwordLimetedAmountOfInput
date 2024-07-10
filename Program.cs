3485352dd2becdf30c481539d7d1e027e5c0a34f
Vladislav Churilov <Nawaharjan@gmail.com>
2024-07-10 20:21:12 +1000

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace passwordLimetedAmountOfInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "12345";
+            string userInput;
+            int tryCount = 3;
+            
+            while (tryCount > 0)
+            {
+                Console.WriteLine("Введите пароль");
+                userInput = Console.ReadLine();
+
+                if (password.Equals(userInput))
+                {
+                    Console.WriteLine("Добро пожаловать!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Пароль неверен =(! У вас осталось " + (tryCount - 1) + " попыток.");
+                    tryCount--;
+
+                    if (tryCount == 0)
+                    {
+                        Console.WriteLine("Ваше количество попыток кончилось, извините и досвидания!");
+                    }
+                }
+            }
+        }
+    }
+}
